8588b47a6f8bd80b3dfa1faeaa97614381b20b92
Farhad Ahmed <farhadahmed2007@gmail.com>
2015-03-30 14:54:54 -0700

    Add classes and objects

--- game.js
+++ game.js
@@ -27,40 +27,42 @@ var thor = new Superhero("Thor", "A mighty hammer");
 var hulk = new Superhero("Hulk", "His fists smash.");
 var hawkeye = new Superhero("Hawkeye", "A bow and arrow");
 
+//Array of heroes.  Their names are the possible answers to the question.
+var heroObjects = [captainAmerica, ironMan, thor, hulk, hawkeye];
+
 //THIS IS THE SCRIPT FOR THE GAME
 var game = function() {
-  var team, guess, answer, message, heroes, counter;
+  var team, guess, answer, message, heroes, counter, answerCAPS;
   counter = 5;
 
-  //Array of heroes.  These are the possible answers to the question.
-  heroes = ['CAPTAINAMERICA', 'IRONMAN', 'THOR', 'HULK', 'HAWKEYE'];
-
-  //Removes all spaces and dashes from user's guess
-  var removeSpaceDash = function() {
-    guess = guess.replace(/\s/, '');
-    guess = guess.replace('-', '');
+  //Removes all spaces and dashes from user's guess or the answer
+  var removeSpaceDash = function(name) {
+    name = name.replace(/\s/, '');
+    name = name.replace('-', '');
   }
 
   //Chooses a random number and then assigns that value in the array to var answer.
-  answer = Math.floor(Math.random() * heroes.length) //0 to 4
-  answer = heroes[answer];
+  answer = Math.floor(Math.random() * heroObjects.length); //0 to 4
+  answer = heroObjects[answer].name;
+  answerCAPS = answer.toUpperCase();
+  removeSpaceDash(answerCAPS);
 
   //Converts users guess to caps and removes all spaces/dashes
   guess = prompt('Which superhero am I thinking of?').toUpperCase();
-  removeSpaceDash();
+  removeSpaceDash(guess);
 
-  if (guess == answer) {
+  if (guess === answerCAPS) {
     message = 'You win! The correct answer is ' + answer + '!';
   }
   else {
     //As long as counter > 0 and guess is incorrect, this code will run and subtract 1 from var counter.
     do {
       guess = prompt('Incorrect. You have ' + counter + ' tries left. \nWhich hero am I thinking of?').toUpperCase();
-      removeSpaceDash();
+      removeSpaceDash(guess);
       counter--;
-    } while (guess !== answer && counter > 0);
+    } while (guess !== answerCAPS && counter > 0);
 
-    if (guess == answer) {
+    if (guess == answerCAPS) {
       message = 'You win! The correct answer is ' + answer + '!';
     }
     else {
